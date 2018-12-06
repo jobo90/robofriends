1165355e8e902c221a008598f9b92aa11e952620
jobo90 <joelbock1@gmail.com>
2018-12-06 21:53:10 +0100

    Updates

---
+++ precache-manifest.c154a53c9f96df77141778a3c4ae3b29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "9a735f7bae5e0696df51",
+    "url": "/robofriends/static/js/main.9a735f7b.chunk.js"
+  },
+  {
+    "revision": "8cc2bdf467f77b5c886b",
+    "url": "/robofriends/static/js/1.8cc2bdf4.chunk.js"
+  },
+  {
+    "revision": "9a735f7bae5e0696df51",
+    "url": "/robofriends/static/css/main.7c59b208.chunk.css"
+  },
+  {
+    "revision": "8cc2bdf467f77b5c886b",
+    "url": "/robofriends/static/css/1.f6cab3db.chunk.css"
+  },
+  {
+    "revision": "daca0ef543270eff91e842e3a52402a6",
+    "url": "/robofriends/index.html"
+  }
+];
